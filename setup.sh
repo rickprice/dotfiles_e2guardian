f6599ccc0471aadd576993376f3f7a64a4951c68
Frederick Price <rprice@pricemail.ca>
2021-02-27 20:01:40 -0500

    Make a fix

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ set -e
 
 ## keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
+## echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 ## Setup dotfiles directories ###
--- setup_os.sh
+++ setup_os.sh
@@ -3,9 +3,9 @@
 # Fail script if any command fails
 set -e
 
-# keep track of the last executed command
+## keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
+## echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Setup Packages for Arch
