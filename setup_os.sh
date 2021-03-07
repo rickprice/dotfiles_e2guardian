dc35574dc180650c6e0db3160b27106d0f08ff46
Frederick Price <rprice@pricemail.ca>
2021-03-07 12:17:50 -0500

    More changes to try and do the MITM setup

--- setup_os.sh
+++ setup_os.sh
@@ -119,14 +119,17 @@ sudo ln -f ~/etc/e2guardian /etc/e2guardian
 sudo useradd --system e2guardian
 
 yay --noconfirm -S e2guardian
-sudo systemctl enable e2guardian.service
-sudo systemctl start e2guardian.service
+
+sudo systemctl stop e2guardian.service
 
 ### Certificate setup
-e2MITMkeys=/etc/e2guardian/private
-e2generatedcerts=/etc/e2guardian/generatedcerts
+e2MITMkeys=~/dotfiles/etc/e2guardian/private
+e2generatedcerts=~/dotfiles/etc/e2guardian/private/generatedcerts
 sudo mkdir -p $e2MITMkeys
 sudo mkdir -p $e2generatedcerts
+sudo chown e2guardian:e2guardian $e2MITMkeys
+sudo chown e2guardian:e2guardian $e2generatedcerts
+
 #### Generate a key for the rootCA
 sudo openssl genrsa 4096 > $e2MITMkeys/private_root.pem
 #### Generate the root CA certificate
@@ -136,3 +139,5 @@ sudo openssl x509 -in $e2MITMkeys/my_rootCA.crt -outform DER -out $e2MITMkeys/my
 #### Generate a key for use with upstream SSL conections
 sudo openssl genrsa 4096 > $e2MITMkeys/private_cert.pem
 
+sudo systemctl enable e2guardian.service
+sudo systemctl start e2guardian.service
