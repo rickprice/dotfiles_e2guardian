cb7e29d9cffee3a3e649a99eaaf4856c7f64eb12
Frederick Price <rprice@pricemail.ca>
2021-03-09 20:08:17 -0500

    Fix how we do the e2guardian setup

--- setup_e2guardian.sh
+++ setup_e2guardian.sh
@@ -15,20 +15,16 @@ yay --noconfirm -S e2guardian
 sudo systemctl stop e2guardian.service
 
 # +++ NOTICE +++ Wipe out existing e2guardian configuration!!!
-if [[ -d /etc/e2guardian ]]
-then
-    sudo rm -rf /etc/e2guardian
-fi
-# +++ NOTICE +++ Wipe out existing e2guardian configuration!!!
 # Put our e2guardian configuration in place
-sudo ln -sf ~/.dotfiles/etc/e2guardian /etc/e2guardian
+sudo cp -Rp ~/.dotfiles/etc/e2guardian /etc/e2guardian
+# +++ NOTICE +++ Wipe out existing e2guardian configuration!!!
 
 ### Certificate setup
 e2MITMkeys=~/.dotfiles/etc/e2guardian/private
 e2generatedcerts=~/.dotfiles/etc/e2guardian/private/generatedcerts
 sudo mkdir -p $e2MITMkeys
 sudo mkdir -p $e2generatedcerts
-sudo chown -R e2guardian:e2guardian ~/.dotfiles/etc/e2guardian
+sudo chown -R e2guardian:e2guardian /etc/e2guardian
 sudo chown -R e2guardian:e2guardian /var/log/e2guardian
 
 #### Generate a key for the rootCA
