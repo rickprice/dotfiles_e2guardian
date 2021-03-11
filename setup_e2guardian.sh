112b02b89c71013a657a20e47e44f6b5ff646d51
Frederick Price <rprice@pricemail.ca>
2021-03-10 19:06:37 -0500

    More e2guardian setup

--- setup_e2guardian.sh
+++ setup_e2guardian.sh
@@ -22,16 +22,22 @@ sudo mkdir -p $e2generatedcerts
 sudo chown -R e2guardian:e2guardian /etc/e2guardian
 sudo chown -R e2guardian:e2guardian /var/log/e2guardian
 
-#### Generate a key for the rootCA
-sudo openssl genrsa -out $e2MITMkeys/private_root.pem 4096 
-#### Generate the root CA certificate
-sudo openssl req -new -x509 -days 3650 -key $e2MITMkeys/private_root.pem -out $e2MITMkeys/my_rootCA.crt
-#### Trust the certificate we generated
-sudo trust anchor $e2MITMkeys/my_rootCA.crt
-#### Create a DER format version of root certificate
-sudo openssl x509 -in $e2MITMkeys/my_rootCA.crt -outform DER -out $e2MITMkeys/my_rootCA.der
-#### Generate a key for use with upstream SSL conections
-sudo openssl genrsa -out $e2MITMkeys/private_cert.pem 4096
+# If MITM root key does not exist, then create new keys
+ if [[ ! -f $e2MITMkeys/private_root.pem ]]
+  then
+    echo "The file ${FILE} does not exist!"
+    #### Generate a key for the rootCA
+    sudo openssl genrsa -out $e2MITMkeys/private_root.pem 4096 
+    #### Generate the root CA certificate
+    sudo openssl req -new -x509 -days 3650 -key $e2MITMkeys/private_root.pem -out $e2MITMkeys/my_rootCA.crt
+    #### Trust the certificate we generated
+    sudo trust anchor $e2MITMkeys/my_rootCA.crt
+    #### Create a DER format version of root certificate
+    sudo openssl x509 -in $e2MITMkeys/my_rootCA.crt -outform DER -out $e2MITMkeys/my_rootCA.der
+    #### Generate a key for use with upstream SSL conections
+    sudo openssl genrsa -out $e2MITMkeys/private_cert.pem 4096
+  fi
+
 sudo systemctl enable e2guardian.service
 sudo systemctl start e2guardian.service
 
