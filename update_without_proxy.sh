163129d7c9a1158a1c2a60e875d87f27f0c438f7
Frederick Price <rprice@pricemail.ca>
2021-03-13 21:40:43 -0500

    Trying to make it easier to update things

--- update_without_proxy.sh
+++ update_without_proxy.sh
@@ -9,3 +9,19 @@ unset all_proxy
 
 ## Now run the git pull
 git pull
+
+sudo systemctl stop e2guardian.service
+
+# Setup enviroment for proxy
+sudo cp etc/environment /etc/environment
+
+# +++ NOTICE +++ Wipe out existing e2guardian configuration!!!
+# Put our e2guardian configuration in place
+sudo cp -Rp etc/e2guardian/* /etc/e2guardian
+# +++ NOTICE +++ Wipe out existing e2guardian configuration!!!
+
+sudo chown -R e2guardian:e2guardian /etc/e2guardian
+sudo chown -R e2guardian:e2guardian /var/log/e2guardian
+
+sudo systemctl enable e2guardian.service
+sudo systemctl start e2guardian.service
